79a9e97a7f9b23cfcca1920b08611aa8cd0c3eca
Andrew Young <andrew@youngshome.com>
2020-11-21 03:15:44 -0800

    manifest logos

--- src/components/ui/app-bar/AppBar.tsx
+++ src/components/ui/app-bar/AppBar.tsx
@@ -6,12 +6,13 @@ import {
 } from '@material-ui/core/styles';
 import {
   AppBar as MaterialAppBar,
-  Toolbar,
+  Tooltip,
 } from '@material-ui/core';
 import {
   Link,
 } from "react-router-dom";
 
+import SourceCodeIcon from '../../../assets/icons/sourcecode.svg';
 import GithubIcon from '../../../assets/icons/github.svg';
 import LinkedInIcon from '../../../assets/icons/linkedin.svg';
 import SpotifyIcon from '../../../assets/icons/spotify.svg';
@@ -65,32 +66,61 @@ function AppBar() {
           </Link>
 
           <div className={classes.iconWrapper}>
-            <a
-              href="https://www.linkedin.com/in/andrew-young-3322b3133/"
-              target="_blank">
-              <img
-                className={classes.icon}
-                src={LinkedInIcon}
-                alt="andrew-young-3322b3133"/>
-            </a>
+            <Tooltip title="Website Source Code">
+              <span>
+                <a
+                  href="https://github.com/andyruwruw/andyruwruw.com"
+                  target="_blank"
+                  rel="noreferrer">
+                  <img
+                    className={classes.icon}
+                    src={SourceCodeIcon}
+                    alt="andyruwruw/andyruwruw.com"/>
+                </a>
+              </span>
+            </Tooltip>
 
-            <a
-              href="https://github.com/andyruwruw"
-              target="_blank">
-              <img
-                className={classes.icon}
-                src={GithubIcon}
-                alt="andyruwruw"/>
-            </a>
+            <Tooltip title="LinkedIn">
+              <span>
+                <a
+                  href="https://www.linkedin.com/in/andrew-young-3322b3133/"
+                  target="_blank"
+                  rel="noreferrer">
+                  <img
+                    className={classes.icon}
+                    src={LinkedInIcon}
+                    alt="andrew-young-3322b3133"/>
+                </a>
+              </span>
+            </Tooltip>
 
-            <a
-              href="https://open.spotify.com/user/12146574234?si=Fec6rYDRSwWxQs07zS892Q"
-              target="_blank">
-              <img
-                className={classes.icon}
-                src={SpotifyIcon}
-                alt="spotify:user:12146574234"/>
-            </a>
+            <Tooltip title="Github">
+              <span>
+                <a
+                  href="https://github.com/andyruwruw"
+                  target="_blank"
+                  rel="noreferrer">
+                  <img
+                    className={classes.icon}
+                    src={GithubIcon}
+                    alt="andyruwruw"/>
+                </a>
+              </span>
+            </Tooltip>
+
+            <Tooltip title="Spotify">
+              <span>
+                <a
+                  href="https://open.spotify.com/user/12146574234?si=Fec6rYDRSwWxQs07zS892Q"
+                  target="_blank"
+                  rel="noreferrer">
+                  <img
+                    className={classes.icon}
+                    src={SpotifyIcon}
+                    alt="spotify:user:12146574234"/>
+                </a>
+              </span>
+            </Tooltip>
           </div>
         </div>
       </MaterialAppBar>
